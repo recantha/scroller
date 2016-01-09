655cb9ded4b94f85ef11b2f9bead3f09cfa3bedd
Michael Horne <mike@recantha.co.uk>
2016-01-09 17:04:08 +0000

    Add web monitor file, update others

--- main.py
+++ main.py
@@ -2,10 +2,16 @@
 
 import os, math, time, scrollphat, sys, socket, psutil
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("gmail.com",80))
-ip = s.getsockname()[0]
-s.close()
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("gmail.com",80))
+	ip = s.getsockname()[0]
+	s.close()
+	time_okay = True
+
+except:
+	ip = "Not connected"
+	time_okay = False
 
 #print(ip)
 
@@ -14,14 +20,15 @@ i = 0
 scrollphat.set_brightness(30)
 
 while True:
-	scrollphat.clear()
-	f = os.popen("date")
-	for i in f.readlines():
-		mytime = i[11:-13]
-	scrollphat.write_string("Time: " + mytime + "     ", 11)
-	for i in range(0, scrollphat.buffer_len() - 11):
-		scrollphat.scroll()
-		time.sleep(0.08)
+	if time_okay:
+		scrollphat.clear()
+		f = os.popen("date")
+		for i in f.readlines():
+			mytime = i[11:-13]
+		scrollphat.write_string("Time: " + mytime + "     ", 11)
+		for i in range(0, scrollphat.buffer_len() - 11):
+			scrollphat.scroll()
+			time.sleep(0.08)
 
 	scrollphat.clear()
 	scrollphat.write_string("IP: " + ip + "    ", 11)
@@ -38,7 +45,7 @@ while True:
 		firstchar  = mytemp[5:-6]
 		secondchar = mytemp[6:-5]
 		thirdchar  = mytemp[8:-3]
-	scrollphat.write_string("System temperature: " + firstchar + secondchar + "." + thirdchar + " C       ", 11)
+	scrollphat.write_string("Sys temp: " + firstchar + secondchar + "." + thirdchar + " C       ", 11)
 	for i in range(0, scrollphat.buffer_len()-11):
 		scrollphat.scroll()
 		time.sleep(0.08)
